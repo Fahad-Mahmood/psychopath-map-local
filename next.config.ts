403f7e50ed9bd0b5ac59c180347d0ef242b5f989
Fahad-Mahmood <fahad492@outlook.com>
2026-05-13 17:48:41 +0500

    fixes

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,17 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-    experimental: {
-        turbo: {
-            rules: {
-                '*.svg': {
-                    loaders: ['@svgr/webpack'],
-                    as: '*.js',
-                },
+    turbopack: {
+        rules: {
+            '*.svg': {
+                loaders: ['@svgr/webpack'],
+                as: '*.js',
             },
         },
     },
-	eslint: {
-		ignoreDuringBuilds: true
-	}
+    eslint: {
+        ignoreDuringBuilds: true
+    }
 }
 
 export default nextConfig
